c60b0347944b1791e63f05c05c5951f54234591d
Nikitagio13 <nikitagio13@gmail.com>
2022-05-17 13:53:33 +0400

    Updated randomizer - added onPress event

--- Games/Sextynine.js
+++ Games/Sextynine.js
@@ -2,29 +2,36 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Image, ImageBackground, TouchableWithoutFeedback, TouchableOpacity } from 'react-native';
 import { randomImages } from '../utilities/Randomizer';
 
-const CardsData = [
-  { image: require('../assets/photos/test4.jpg'), id: 1 },
-  { image: require('../assets/photos/test7.jpg'), id: 2 },
-  { image: require('../assets/photos/test.jpg'), id: 3 },
-  { image: require('../assets/photos/test2.jpg'), id: 4 },
-  { image: require('../assets/photos/test3.jpg'), id: 5 },
-  { image: require('../assets/photos/test5.jpg'), id: 6 },
-  { image: require('../assets/photos/test6.jpg'), id: 7 },
-];
-
-
 
 export default function Sextynine() {
+
+  const [cardsData, setCardsData] = useState([
+    { image: require('../assets/photos/test4.jpg'), id: 1 },
+    { image: require('../assets/photos/test7.jpg'), id: 2 },
+    { image: require('../assets/photos/test.jpg'), id: 3 },
+    { image: require('../assets/photos/test2.jpg'), id: 4 },
+    { image: require('../assets/photos/test3.jpg'), id: 5 },
+    { image: require('../assets/photos/test5.jpg'), id: 6 },
+    { image: require('../assets/photos/test6.jpg'), id: 7 },
+  ]);
+
+  const onPressF = (image) => {
+    setCardsData((prevImage) => {
+      
+      return prevImage.filter(img => img.image != image);
+    })
+    
+  }
 
   return (
     <View>
       <ImageBackground source={require('../assets/photos/backgroundcolors.jpg')} style={styles.image} />
-      <TouchableOpacity>
+      <TouchableWithoutFeedback onPress={() => onPressF(cardsData.image)}>
         <View style={styles.cardImages}>
 
-          <Image source={randomImages(CardsData).image} style={styles.cardsFromData} />
+          <Image source={randomImages(cardsData).image} style={styles.cardsFromData} />
         </View>
-      </TouchableOpacity>
+      </TouchableWithoutFeedback>
     </View>
   )
 }
@@ -79,9 +86,9 @@ const styles = StyleSheet.create({
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
-    width: 200,
-    height: 500,
-    zIndex: 1,
+    width: "85%",
+    height: "96%",
+   borderRadius: 10,
   }
 
 
